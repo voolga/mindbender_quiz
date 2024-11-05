0c41499a95d91128e7be7bdf74d13a4660cac09e
Olga <olga.volotovich@mail.ru>
2024-11-05 22:39:55 +0100

    fix: route changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://voolga.github.io/mindbender_quiz/',
+  base: '/mindbender_quiz/',
   plugins: [react()]
 })
